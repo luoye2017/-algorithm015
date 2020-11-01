99b177c7f923790553b1d3b5c47faf72b1738f0e
luoye <wangfan1996love@163.com>
2020-11-01 23:35:38 +0800

    提交作业

--- Week_09/README.md
+++ Week_09/README.md
@@ -1,1 +1,25 @@
 学习笔记
+
+### 感触
+
+1. 人肉递归低效，很累
+2. 找到最近最简方法，将其拆解成可重复解决的问题
+3. 数学归纳法思维
+
+本质：寻找重复性 -> 计算机指令集
+
+### 动态规划 Dynamic Programming
+
+1. Simplifying a complicated problem by breaking it down into simpler sub-problems（in a recursive manner）
+2. Divide & Conquer + Optimal substructure 分治 + 最优子结构
+3. 顺推形式 ： 动态递推
+
+
+
+### 关键点
+
+动态规划和递归或者分治没有根本上的区别（关键是看有没有最优子结构）
+
+拥有共性：找到重复子问题
+
+差异性：最优子结构，中途可以淘汰次优解
---
+++ Week_09/first_unique_character_in_a_string.java
@@ -0,0 +1,23 @@
+package com.example.leetcode.leetcode;
+
+import java.util.HashMap;
+
+public class first_unique_character_in_a_string {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+}
